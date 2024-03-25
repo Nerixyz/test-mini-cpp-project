f9df35c2d93d05d09df0cf56700c69e3d58cb604
Nerixyz <nerixdev@outlook.de>
2024-03-25 17:25:16 +0100

    chore: more test

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ int main(int argc, char **argv)
 {
     auto test = new std::string;  // :)
     std::cout << "Hello xD\n";
-    auto test2 = new std::string;  // :)
-    auto test3 = new std::string;  // :)
-    auto test4 = new std::string;  // :)
+    auto test2 = new std::string("xd");  // :)
+    auto test3 = new std::string;        // :)
+    auto test4 = new std::string;        // :)
     return 0;
 }
 
