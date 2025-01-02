61e9eb730247a63bc2341cda05a04f6c50057558
Nerixyz <nerixdev@outlook.de>
2025-01-02 20:28:05 +0100

    j

--- src/loading.hpp
+++ src/loading.hpp
@@ -35,7 +35,7 @@ namespace loading {
 inline void loadLibrary(const std::filesystem::path &path)
 {
     std::cout << "loading " << path << '\n';
-    auto *addr = ::dlopen(path.c_str(), 0);
+    auto *addr = ::dlopen(path.c_str(), RTLD_NOW | RTLD_GLOBAL);
     if (addr == nullptr)
     {
         std::cout << "loading failed:" << ::dlerror() << '\n';
