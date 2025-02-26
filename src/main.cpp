743655f1d922e59c22fab0a248202c00003757e9
Nerixyz <nerixdev@outlook.de>
2025-02-26 17:20:02 +0100

    init

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ void myFunction()
 
 int main(int argc, char **argv)
 {
-    auto test = new std::string;  // :)
+    auto test = new std::string;  // :(
     std::cout << "Hello xD\n";
     auto test2 = new std::string;  // :)
-    auto test3 = new std::string;  // :)
+    auto test3 = new std::string;  // :(
     auto test4 = new std::string;  // :)
     return 0;
 }
