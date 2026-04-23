b7fccc6ae3b96b25add24efbf0ddebc2ef304807
Nerixyz <nerixdev@outlook.de>
2026-04-23 20:35:19 +0200

    revert the other thing

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,7 @@ int main(int argc, char **argv)
     std::cout << "Hello xD\n";
     auto test2 = new std::string;  // :)
     auto test3 = new std::string;  // :)
-    auto test4 = new std::string;  // :
-    auto foo = new QString;
+    auto test4 = new std::string;  // :)
     return 0;
 }
 
